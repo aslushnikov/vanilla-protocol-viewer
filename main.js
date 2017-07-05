ad017dd60fc7db8134641c0b8cee4982b1c38bc2
Andrey Lushnikov <lushnikov@chromium.org>
2017-07-05 21:06:53 +0300

    move protocol renderer methods under the namespace

--- main.js
+++ main.js
@@ -65,7 +65,7 @@ async function main() {
       link.classList.add('active-link');
 
     content.innerHTML = '';
-    let e = renderDomain(domains.get(domain));
+    let e = Renderer.renderDomain(domains.get(domain));
     content.appendChild(e);
     let elem = document.getElementById(route);
     if (elem)
@@ -85,191 +85,6 @@ function renderSidebar(domainNames) {
     a.href = '#' + name;
     a.textContent = name;
   }
-}
-
-function renderDomain(domain) {
-  let main = E.div('domain');
-
-  {
-    // Render domain main description.
-    let container = main.div('box');
-
-    let title = container.el('h2');
-    title.textContent = domain.domain;
-
-    let description = container.el('p');
-    description.innerHTML = domain.description || '';
-    if (domain.experimental)
-      description.appendChild(experimentalMark());
-  }
-
-  if (domain.commands && domain.commands.length) {
-    // Render methods.
-    let title = main.el('h3');
-    title.textContent = 'Methods';
-    let container = main.box('box');
-    for (let i = 0; i < domain.commands.length; ++i) {
-      if (i !== 0)
-        container.div('boundary');
-      let method = domain.commands[i];
-      container.appendChild(renderEventOrMethod(domain, method));
-    }
-  }
-
-  if (domain.events && domain.events.length) {
-    // Render events.
-    let title = main.el('h3');
-    title.textContent = 'Events';
-    let container = main.box('box');
-    for (let i = 0; i < domain.events.length; ++i) {
-      if (i !== 0)
-        container.div('boundary');
-      let event = domain.events[i];
-      container.appendChild(renderEventOrMethod(domain, event));
-    }
-  }
-
-  if (domain.types && domain.types.length) {
-    // Render events.
-    let title = main.el('h3');
-    title.textContent = 'Types';
-    let container = main.box('box');
-    for (let i = 0; i < domain.types.length; ++i) {
-      if (i !== 0)
-        container.div('boundary');
-      let type = domain.types[i];
-      container.appendChild(renderType(domain, type));
-    }
-  }
-
-  return main;
-}
-
-function renderType(domain, type) {
-  let main = E.div('type');
-  main.appendChild(renderTitle(domain.domain, type.id));
-  {
-    // Render description.
-    let p = main.el('p');
-    p.textContent = type.description || '';
-    if (type.experimental)
-      p.appendChild(experimentalMark());
-  }
-  if (type.properties && type.properties.length) {
-    // Render parameters.
-    let title = main.el('h5');
-    title.textContent = 'Properties';
-    let container = main.el('dl', 'parameter-list');
-    for (let parameter of type.properties)
-      container.appendChild(renderParameter(domain, parameter));
-  }
-  if (type.type) {
-    main.el('p', '', 'Type: ')
-      .text(type.type, 'parameter-type');
-  }
-  if (type.enum) {
-    main.el('h5', '', 'Allowed values');
-    main.el('p', '', type.enum.join(', '));
-  }
-  return main;
-}
-
-function renderTitle(domainName, domainEntry) {
-    // Render heading.
-    let heading = E.el('h4', 'monospace');
-    let id = `${domainName}.${domainEntry}`;
-    heading.setAttribute('id', id);
-    heading.text(domainName + '.', 'method-domain');
-    heading.text(domainEntry, 'method-name');
-    heading.a('#', '#' + id).classList.add('title-link');
-    return heading;
-}
-
-function renderEventOrMethod(domain, method) {
-  let main = E.div('method');
-  main.appendChild(renderTitle(domain.domain, method.name));
-  {
-    // Render description.
-    let p = main.el('p');
-    p.innerHTML = method.description || '';
-    if (method.experimental)
-      p.appendChild(experimentalMark());
-  }
-  if (method.parameters && method.parameters.length) {
-    // Render parameters.
-    let title = main.el('h5');
-    title.textContent = 'Parameters';
-    let container = main.el('dl', 'parameter-list');
-    for (let parameter of method.parameters)
-      container.appendChild(renderParameter(domain, parameter));
-  }
-  if (method.returns && method.returns.length) {
-    // Render return values.
-    let title = main.el('h5');
-    title.textContent = 'RETURN OBJECT';
-    let container = main.el('dl', 'parameter-list');
-    for (let parameter of method.returns)
-      container.appendChild(renderParameter(domain, parameter));
-  }
-  return main;
-}
-
-function renderParameter(domain, parameter) {
-  let main = E.hbox('parameter');
-  {
-    // Render parameter name.
-    let name = main.div('parameter-name monospace');
-    if (parameter.optional)
-      name.classList.add('optional');
-    name.textContent = parameter.name;
-  }
-  {
-    // Render parameter value.
-    let container = main.vbox('parameter-value');
-    container.appendChild(renderTypeLink(domain, parameter));
-    let description = container.span('parameter-description');
-    description.innerHTML = parameter.description || '';
-    if (parameter.experimental)
-      description.appendChild(experimentalMark());
-  }
-  return main;
-}
-
-var primitiveTypes = new Set([
-  "string",
-  "integer",
-  "boolean",
-  "number",
-  "object",
-  "any"
-]);
-
-function renderTypeLink(domain, parameter) {
-  if (primitiveTypes.has(parameter.type))
-    return E.el('span', 'parameter-type', parameter.type);
-  if (parameter.$ref) {
-    let a = E.el('a', 'parameter-type');
-    if (parameter.$ref.includes('.'))
-      a.href = '#' + parameter.$ref;
-    else
-      a.href = '#' + domain.domain + '.' + parameter.$ref;
-    a.textContent = parameter.$ref;
-    return a;
-  }
-  if (parameter.type === 'array') {
-    let generic = E.span('parameter-type');
-    generic.text('array [ ');
-    generic.appendChild(renderTypeLink(domain, parameter.items));
-    generic.text(' ]');
-    return generic;
-  }
-  return E.el('span', 'parameter-type', '<TYPE>');
-}
-
-function experimentalMark() {
-  let e = E.el('span', 'experimental', 'experimental');
-  e.title = 'This may be changed, moved or removed';
-  return e;
 }
 
 function renderError(error) {
@@ -324,6 +139,193 @@ function renderLanding(loadingTime, protocols) {
   }
   return main;
 }
+
+const Renderer = {
+  renderDomain: function(domain) {
+    let main = E.div('domain');
+
+    {
+      // Render domain main description.
+      let container = main.div('box');
+
+      let title = container.el('h2');
+      title.textContent = domain.domain;
+
+      let description = container.el('p');
+      description.innerHTML = domain.description || '';
+      if (domain.experimental)
+        description.appendChild(Renderer.experimentalMark());
+    }
+
+    if (domain.commands && domain.commands.length) {
+      // Render methods.
+      let title = main.el('h3');
+      title.textContent = 'Methods';
+      let container = main.box('box');
+      for (let i = 0; i < domain.commands.length; ++i) {
+        if (i !== 0)
+          container.div('boundary');
+        let method = domain.commands[i];
+        container.appendChild(Renderer.renderEventOrMethod(domain, method));
+      }
+    }
+
+    if (domain.events && domain.events.length) {
+      // Render events.
+      let title = main.el('h3');
+      title.textContent = 'Events';
+      let container = main.box('box');
+      for (let i = 0; i < domain.events.length; ++i) {
+        if (i !== 0)
+          container.div('boundary');
+        let event = domain.events[i];
+        container.appendChild(Renderer.renderEventOrMethod(domain, event));
+      }
+    }
+
+    if (domain.types && domain.types.length) {
+      // Render events.
+      let title = main.el('h3');
+      title.textContent = 'Types';
+      let container = main.box('box');
+      for (let i = 0; i < domain.types.length; ++i) {
+        if (i !== 0)
+          container.div('boundary');
+        let type = domain.types[i];
+        container.appendChild(Renderer.renderDomainType(domain, type));
+      }
+    }
+
+    return main;
+  },
+
+  renderDomainType: function(domain, type) {
+    let main = E.div('type');
+    main.appendChild(Renderer.renderTitle(domain.domain, type.id));
+    {
+      // Render description.
+      let p = main.el('p');
+      p.textContent = type.description || '';
+      if (type.experimental)
+        p.appendChild(Renderer.experimentalMark());
+    }
+    if (type.properties && type.properties.length) {
+      // Render parameters.
+      let title = main.el('h5');
+      title.textContent = 'Properties';
+      let container = main.el('dl', 'parameter-list');
+      for (let parameter of type.properties)
+        container.appendChild(Renderer.renderParameter(domain, parameter));
+    }
+    if (type.type) {
+      main.el('p', '', 'Type: ')
+        .text(type.type, 'parameter-type');
+    }
+    if (type.enum) {
+      main.el('h5', '', 'Allowed values');
+      main.el('p', '', type.enum.join(', '));
+    }
+    return main;
+  },
+
+  renderTitle: function(domainName, title) {
+    // Render heading.
+    let heading = E.el('h4', 'monospace');
+    let id = `${domainName}.${title}`;
+    heading.setAttribute('id', id);
+    heading.text(domainName + '.', 'method-domain');
+    heading.text(title, 'method-name');
+    heading.a('#', '#' + id).classList.add('title-link');
+    return heading;
+  },
+
+  renderEventOrMethod: function(domain, method) {
+    let main = E.div('method');
+    main.appendChild(Renderer.renderTitle(domain.domain, method.name));
+    {
+      // Render description.
+      let p = main.el('p');
+      p.innerHTML = method.description || '';
+      if (method.experimental)
+        p.appendChild(Renderer.experimentalMark());
+    }
+    if (method.parameters && method.parameters.length) {
+      // Render parameters.
+      let title = main.el('h5');
+      title.textContent = 'Parameters';
+      let container = main.el('dl', 'parameter-list');
+      for (let parameter of method.parameters)
+        container.appendChild(Renderer.renderParameter(domain, parameter));
+    }
+    if (method.returns && method.returns.length) {
+      // Render return values.
+      let title = main.el('h5');
+      title.textContent = 'RETURN OBJECT';
+      let container = main.el('dl', 'parameter-list');
+      for (let parameter of method.returns)
+        container.appendChild(Renderer.renderParameter(domain, parameter));
+    }
+    return main;
+  },
+
+  renderParameter: function(domain, parameter) {
+    let main = E.hbox('parameter');
+    {
+      // Render parameter name.
+      let name = main.div('parameter-name monospace');
+      if (parameter.optional)
+        name.classList.add('optional');
+      name.textContent = parameter.name;
+    }
+    {
+      // Render parameter value.
+      let container = main.vbox('parameter-value');
+      container.appendChild(Renderer.renderTypeLink(domain, parameter));
+      let description = container.span('parameter-description');
+      description.innerHTML = parameter.description || '';
+      if (parameter.experimental)
+        description.appendChild(Renderer.experimentalMark());
+    }
+    return main;
+  },
+
+  renderTypeLink: function(domain, parameter) {
+    const primitiveTypes = new Set([
+      "string",
+      "integer",
+      "boolean",
+      "number",
+      "object",
+      "any"
+    ]);
+
+    if (primitiveTypes.has(parameter.type))
+      return E.span('parameter-type', parameter.type);
+    if (parameter.$ref) {
+      let a = E.el('a', 'parameter-type');
+      if (parameter.$ref.includes('.'))
+        a.href = '#' + parameter.$ref;
+      else
+        a.href = '#' + domain.domain + '.' + parameter.$ref;
+      a.textContent = parameter.$ref;
+      return a;
+    }
+    if (parameter.type === 'array') {
+      let generic = E.span('parameter-type');
+      generic.text('array [ ');
+      generic.appendChild(Renderer.renderTypeLink(domain, parameter.items));
+      generic.text(' ]');
+      return generic;
+    }
+    return E.el('span', 'parameter-type', '<TYPE>');
+  },
+
+  experimentalMark: function() {
+    let e = E.el('span', 'experimental', 'experimental');
+    e.title = 'This may be changed, moved or removed';
+    return e;
+  },
+};
 
 // HELPERS
 
